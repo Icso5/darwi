4ce361022e12110a7f7385b2cacc67e0e071acbf
Icso5 <97122553+Icso5@users.noreply.github.com>
2022-01-04 12:45:54 -0600

    Add files via upload

---
+++ tiamo.py
@@ -0,0 +1,15 @@
+#bashrunner
+print('by ixo')
+print('-----------')
+print('saludos desde tu ano')
+print('-------------------------')
+answere = input('darwin quieres ser mi novio?\n')
+print('----------------')
+print('si dijiste si')
+answere = print('tiamo') 
+print('🖕🖕🖕🖕🖕🖕🖕🖕🖕')
+
+
+
+print('si dijiste no')
+answere = print('chinga a tu madre')
